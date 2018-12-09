5f348a3c694f80d5943f4081a1155aff3f4e1037
Rob Bekkering <rob@no9.com>
2018-12-09 23:40:25 +0100

    Continued with responsiveness issues and added Burger to navbar

---
+++ Javascript/navbar-dropdown.js
@@ -0,0 +1,14 @@
+const navBurger = document.querySelector('#nav-burger');
+const navDropdown = document.querySelector('#nav-dropdown');
+const navDropdownLinks = document.querySelectorAll('.nav-dropdown-link');
+
+
+navBurger.addEventListener('click', () => {
+  navDropdown.classList.toggle('dropdown-toggle')
+})
+
+navDropdownLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    navDropdown.classList.add('dropdown-toggle')
+  })
+});
